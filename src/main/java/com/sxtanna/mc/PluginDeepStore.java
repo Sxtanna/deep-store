8020e5d2edfda19bb95ff8a44f43b69b2139e27c
Sxtanna <ranald.taylor@gmail.com>
2020-08-01 06:15:13 -0400

    Added description, and plugin class.

---
+++ src/main/java/com/sxtanna/mc/PluginDeepStore.java
@@ -0,0 +1,26 @@
+package com.sxtanna.mc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginDeepStore extends JavaPlugin
+{
+
+	@Override
+	public void onLoad()
+	{
+
+	}
+
+	@Override
+	public void onEnable()
+	{
+
+	}
+
+	@Override
+	public void onDisable()
+	{
+
+	}
+
+}
