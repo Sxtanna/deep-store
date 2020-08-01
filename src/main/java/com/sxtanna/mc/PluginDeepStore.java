3a3b651cf88403a6ebac5cf697490505b0c84c27
Sxtanna <ranald.taylor@gmail.com>
2020-08-01 08:01:58 -0400

    added module

--- src/main/java/com/sxtanna/mc/PluginDeepStore.java
+++ src/main/java/com/sxtanna/mc/PluginDeepStore.java
@@ -1,9 +1,15 @@
 package com.sxtanna.mc;
 
+import com.sxtanna.mc.mods.ModuleDeepStore;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public final class PluginDeepStore extends JavaPlugin
 {
+
+	@NotNull
+	private final ModuleDeepStore module = new ModuleDeepStore(this);
+
 
 	@Override
 	public void onLoad()
@@ -14,13 +20,20 @@ public final class PluginDeepStore extends JavaPlugin
 	@Override
 	public void onEnable()
 	{
-
+		module.load();
 	}
 
 	@Override
 	public void onDisable()
 	{
+		module.kill();
+	}
 
+
+	@NotNull
+	public ModuleDeepStore getModule()
+	{
+		return module;
 	}
 
 }
